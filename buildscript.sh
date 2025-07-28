2a4847ac16435afb3dfdb89b9fda289cc8234327
SomduttSharmaa <somdutt7080@gmail.com>
2025-07-28 13:12:24 +0530

    fix: correct exit code in buildcheck.sh

--- buildscript.sh
+++ buildscript.sh
@@ -4,8 +4,8 @@ echo "VERCEL_GIT_COMMIT_REF: $VERCEL_GIT_COMMIT_REF"
 
 if [[ "$VERCEL_GIT_COMMIT_REF" == "main" || "$VERCEL_GIT_COMMIT_REF" == "dev" ]]; then
   echo "✅ - Build can proceed ($VERCEL_GIT_COMMIT_REF branch)"
-  exit 1
+  exit 0   # success
 else
   echo "🛑 - Build cancelled (not main or dev branch)"
-  exit 0
+  exit 1   # fail
 fi
